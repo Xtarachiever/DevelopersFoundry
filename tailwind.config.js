273ea657f3dc62f4807030bebd77af497e0cc3aa
Xtarachiever <estheradeyemi796@gmail.com>
2024-12-31 10:46:26 +0100

    Initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+      'primary_dark': '#09080A',
+      'gray':'#393449',
+      'brown':'#582411',
+      'light_brown':'#AA4929',
+      'dark_gray':'#5E565A',
+      'dull_gray':'#939285',
+      'stained_white':'#C3C3C2',
+      'gray2':'#4347C1'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
